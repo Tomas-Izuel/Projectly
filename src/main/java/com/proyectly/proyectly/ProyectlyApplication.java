6911eeca82fb8df6194f764a08c85b30e3efac02
Tomas Izuel <tomasizuel@gmail.com>
2023-02-27 12:14:08 -0300

    starting proyect with springboot

---
+++ src/main/java/com/proyectly/proyectly/ProyectlyApplication.java
@@ -0,0 +1,11 @@
+package com.proyectly.proyectly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectlyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectlyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/proyectly/proyectly/ProyectlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectly.proyectly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
